45aabbc20960fc9257e0b7add949556e03354f14
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2019-09-20 15:15:45 +0300

    Add package to mix.exs

--- mix.exs
+++ mix.exs
@@ -5,6 +5,7 @@ defmodule AbsintheCache.MixProject do
     [
       app: :absinthe_cache,
       version: "0.1.0",
+      package: package(),
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -21,8 +22,16 @@ defmodule AbsintheCache.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package() do
+    [
+      description: "Provides caching functionality on top of Absinthe GraphQL Server",
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE* CHANGELOG* ),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/IvanIvanoff/absinthe_cache"}
     ]
   end
 end
